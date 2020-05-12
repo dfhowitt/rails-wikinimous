e1179ff12ad637827f09f2c3176d6e2474ea8f0f
Daniel Howitt <daniel.howittcontact@gmail.com>
2020-05-12 17:50:39 +0100

    Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,5 +3,11 @@
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }]
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  Article.create(
+    title: Faker::JapaneseMedia::DragonBall.character,
+    content: Faker::Quote.famous_last_words
+  )
+end
